ae6f9964316f09f1aa7aa30dbec2e241a51fee36
chun-smg <chun.fore@gmail.com>
2023-06-19 01:33:38 +0900

    課題の完了処理(未完)

--- functions/firestore_write.js
+++ functions/firestore_write.js
@@ -50,3 +50,25 @@ exports.add_class_name_data = async({class_code="", class_name=""}) => {
   },{ merge: true})
   return Promise.resolve("done");
 }
+
+// exports.save_task = async({user_id, class_name, class_code, user_name}) => {
+//   await db.collection("tasks").doc(user_id).set({
+//     class_name: `${class_name}`,
+//     display: false, 
+//     finish:
+//     serial_id:
+//     task_limit:
+//     task_name:
+//   });
+// }
+
+exports.set_task_status = async({user_id="", is_finish=null, task_serial_id=0}) =>{
+  if (user_id == ""|| is_finish == null || task_serial_id == 0){
+    return Promise.reject(new Error("Parameter not found"));
+  }
+  await db.collection("tasks").doc(user_id).update({
+    //このままだと授業名指定されていないので入りません(要改善)
+    display: is_finish
+
+  })
+}

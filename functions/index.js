8fdb96934bbd6f65f2ccb987f568d358e6ecba59
yuto0727 <wy0727.watyu20@gmail.com>
2023-06-29 10:04:43 +0900

    エンドポイント公開関連処理のみに変更

--- functions/index.js
+++ functions/index.js
@@ -1,65 +1,45 @@
+// Firebase関連設定
 require("firebase-functions/logger/compat");
-const express = require("express");
-const linebot_sdk = require("@line/bot-sdk");
 const {initializeApp, cert} = require("firebase-admin/app");
 const firebase_functions = require("firebase-functions");
-
-const line_sender = require("./file_modules/line_message_sender");
-const firestore_read = require("./file_modules/firestore_read");
-const firebase_write = require("./file_modules/firestore_write");
-
-// ----------------------------------------------
-// 初期化処理
-// ----------------------------------------------
-// firebaseクライアント
 const serviceAccount = require("./keys/ServiceAccount.json");
 initializeApp({credential: cert(serviceAccount)});
 // firebase_initializeApp(); // 本番デプロイ時は上を無効化してこっち
 
-// LINEクライアント
+// LINEbotSDK
+const linebot_sdk = require("@line/bot-sdk");
 const linebot_account = require("./keys/LineAccount.json");
 const linebot_client = new linebot_sdk.Client(linebot_account);
 
-// LINEエンドポイント用Expressフレームワーク
+// Express
+const express = require("express");
 const app = express();
 
-
-// ----------------------------------------------
-// ユーザーメッセージ処理
-// ----------------------------------------------
-const message_handler = async(event_data) => {
-  // テキストメッセージ以外を除外
-  console.log("type1:", event_data.type, "type2", event_data.message.type);
-  if (event_data.type == "message" && event_data.message.type !== "text"){
-    line_sender.alert_only_text({
-      client: linebot_client,
-      reply_token: event_data.replyToken
-    })
-    return Promise.resolve("done");
-  }
-
-  line_sender.text_parrot_returner({
-    client: linebot_client,
-    reply_token: event_data.replyToken,
-    message: event_data.message.text
-  })
-
-  return Promise.resolve("done");
-}
-
+// メッセージハンドラ
+const message_handler = require("./file_modules/message_handler")
 
 // ----------------------------------------------
 // エンドポイント公開設定
 // ----------------------------------------------
 app.post('/webhook', linebot_sdk.middleware(linebot_account), (req, res) => {
-  // console.log(req.body.events);
-  Promise.all(req.body.events.map(message_handler))
-  .then(() => {
-    res.status(200).end();
-  })
-  .catch((error) => {
+  message_handler.set_client({
+    client: linebot_client,
+    reply_token: req.body.events[0].replyToken
+
+  }).then(() => {
+    message_handler.handle({
+      event_data: req.body.events[0]
+    })
+
+  }).then(() => {
+    res.status(200).json({}).end();
+
+  }).catch((error) => {
+    message_handler.error({
+      err: error
+    })
     console.error(error);
-    res.status(200).end();
+    res.status(200).json({}).end();
   })
 });
 

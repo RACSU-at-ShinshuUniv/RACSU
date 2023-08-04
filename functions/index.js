33b07c07cc057d7fe0126dba55ef7a1ad21daaa3
yuto0727 <wy0727.watyu20@gmail.com>
2023-08-04 17:11:29 +0900

    レイテンシテスト

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 // Firebase関連設定
 require("firebase-functions/logger/compat");
+
 const {initializeApp, cert} = require("firebase-admin/app");
-const firebase_functions = require("firebase-functions");
-const serviceAccount = require("./keys/ServiceAccount.json");
-initializeApp({credential: cert(serviceAccount)});
-// initializeApp(); // 本番デプロイ時は上を無効化してこっち
+const functions = require("firebase-functions");
+const { getFirestore } = require('firebase-admin/firestore');
+// const serviceAccount = require("./keys/ServiceAccount.json");
+// initializeApp({credential: cert(serviceAccount)});
+initializeApp(); // 本番デプロイ時は上を無効化してこっち
 
 // LINE_bot_SDKインスタンス作成
 const linebot_sdk = require("@line/bot-sdk");
@@ -15,40 +17,48 @@ const linebot_client = new linebot_sdk.Client(linebot_account);
 const express = require("express");
 const app = express();
 
-// メッセージハンドラ
-// const message_handler = require("./file_modules/message_handler")
-const message_handler = require("./file_modules/dev_message_repeater")
+// dev
+const db = getFirestore();
+// /dev
+
 
 // タイムゾーン設定
 process.env.TZ = "Asia/Tokyo";
 
+n = 0;
+
 // ----------------------------------------------
 // エンドポイント公開設定
 // ----------------------------------------------
-app.post('/webhook', linebot_sdk.middleware(linebot_account), (req, res) => {
-  message_handler.set_client({
-    client: linebot_client,
-    reply_token: req.body.events[0].replyToken
+app.post('/webhook', linebot_sdk.middleware(linebot_account), async(req, res) => {
+  n += 1;
+  console.log(`@@@function start | count: ${n}times`);
 
-  }).then(() => {
-    message_handler.handle({
-      event_data: req.body.events[0]
-    })
+  console.time("firestore_init")
+  const doc_data = await db.collection("flex").doc("U5a2991011c7a349ab5c5bebc4347cfb6").get();
+  console.timeEnd("firestore_init")
 
-  }).then(() => {
-    res.status(200).json({}).end();
 
-  }).catch((error) => {
-    message_handler.error({
-      err: error
-    })
-    console.error(error);
-    res.status(200).json({}).end();
-  })
+  linebot_client.replyMessage(req.body.events[0].replyToken, {
+    type: "flex",
+    altText: doc_data.data().alt_text,
+    contents: {
+      "type": "bubble",
+      "size": "giga",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": doc_data.data().contents
+      }
+    }
+  });
+
+  res.status(200).json({}).end();
 });
 
-exports.line_end_point = firebase_functions.runWith({
+exports.line_end_point = functions
+.region('asia-northeast1')
+.runWith({
   maxInstances: 1,
-  timeoutSeconds: 30,
-  memory: "256MB",
+  memory: "1GB",
 }).https.onRequest(app);

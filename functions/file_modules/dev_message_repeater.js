9d779d42deb12ae3c4ddb2de297813f10ff2b829
yuto0727 <wy0727.watyu20@gmail.com>
2023-08-04 17:11:49 +0900

    処理時間計測版

--- functions/file_modules/dev_message_repeater.js
+++ functions/file_modules/dev_message_repeater.js
@@ -1,4 +1,5 @@
 const Line_Sender = require("./line_sender");
+const firestore_read = require("./firestore_read");
 
 exports.set_client = async({client=null, reply_token=""}) => {
   this.line_sender = new Line_Sender({
@@ -11,8 +12,20 @@ exports.handle = async({event_data=""}) => {
   if (event_data.type == "message"){
     // テキストメッセージのみ処理
     if (event_data.message.type == "text"){
-      this.line_sender.text({
-        message: `${event_data.message.text}を受け取りました。`
+
+      console.time('my-timer');
+
+      const contents = await firestore_read.get_data({
+        collection: "flex",
+        doc: "U5a2991011c7a349ab5c5bebc4347cfb6"
+      })
+
+      console.timeEnd('my-timer');
+
+
+      this.line_sender.flex_task_list({
+        contents: contents.contents,
+        alt_text: contents.alt_text
       });
     }
   }

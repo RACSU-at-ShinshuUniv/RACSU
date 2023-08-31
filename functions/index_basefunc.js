0763d484efe2ee947941d9e6114af6f0515dabf4
yuto0727 <wy0727.watyu20@gmail.com>
2023-09-01 00:44:12 +0900

    オウム返しの基本機能に変更

--- functions/index_basefunc.js
+++ functions/index_basefunc.js
@@ -3,10 +3,9 @@ require("firebase-functions/logger/compat");
 
 const {initializeApp, cert} = require("firebase-admin/app");
 const functions = require("firebase-functions");
-const { getFirestore } = require('firebase-admin/firestore');
-// const serviceAccount = require("./keys/ServiceAccount.json");
-// initializeApp({credential: cert(serviceAccount)});
-initializeApp(); // 本番デプロイ時は上を無効化してこっち
+const serviceAccount = require("./keys/ServiceAccount.json");
+initializeApp({credential: cert(serviceAccount)});
+// initializeApp(); // 本番デプロイ時は上を無効化してこっち
 
 // LINE_bot_SDKインスタンス作成
 const linebot_sdk = require("@line/bot-sdk");
@@ -17,40 +16,18 @@ const linebot_client = new linebot_sdk.Client(linebot_account);
 const express = require("express");
 const app = express();
 
-// dev
-const db = getFirestore();
-// /dev
-
 
 // タイムゾーン設定
 process.env.TZ = "Asia/Tokyo";
-
-n = 0;
 
 // ----------------------------------------------
 // エンドポイント公開設定
 // ----------------------------------------------
 app.post('/webhook', linebot_sdk.middleware(linebot_account), async(req, res) => {
-  n += 1;
-  console.log(`@@@function start | count: ${n}times`);
-
-  console.time("firestore_init")
-  const doc_data = await db.collection("flex").doc("U5a2991011c7a349ab5c5bebc4347cfb6").get();
-  console.timeEnd("firestore_init")
-
 
   linebot_client.replyMessage(req.body.events[0].replyToken, {
-    type: "flex",
-    altText: doc_data.data().alt_text,
-    contents: {
-      "type": "bubble",
-      "size": "giga",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": doc_data.data().contents
-      }
-    }
+    type: "text",
+    text: "あああ"
   });
 
   res.status(200).json({}).end();

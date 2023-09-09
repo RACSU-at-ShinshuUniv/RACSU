c95a6eec103fc75d9d53c3a10443729d5748e3fe
yuto0727 <wy0727.watyu20@gmail.com>
2023-09-09 23:26:26 +0900

    データベース更新時のマージ処理を追加

--- functions/apps/app_update_task.js
+++ functions/apps/app_update_task.js
@@ -1,5 +1,5 @@
 module.exports = async(db, {user_id="", account_data={}}) => {
-  // ical取得先URLを取得
+  // ユーザーデータからical取得先URLに変換
   const term = new Date();
   term.setMonth(term.getMonth()-3);
   const url_g = `https://lms.ealps.shinshu-u.ac.jp/${term.getFullYear()}/g/calendar/export_execute.php?userid=${account_data.moodle_general_id}&authtoken=${account_data.moodle_general_token}&preset_what=all&preset_time=recentupcoming`;
@@ -19,7 +19,6 @@ module.exports = async(db, {user_id="", account_data={}}) => {
   // icalデータをFirestore保存形式に変換
   const data_formatter = require("../file_modules/data_formatter");
   const class_name_dic = (await db.collection("overall").doc("classes").get()).data();
-
   const task_data_general = await data_formatter.ical_to_json(db, {
     class_name_dic: class_name_dic,
     ical_data: ical_data_general
@@ -29,18 +28,36 @@ module.exports = async(db, {user_id="", account_data={}}) => {
     ical_data: ical_data_specific
   });
 
+  // icalからJsonに変換したデータと、すでにデータベースに登録済みの課題データをマージする
+  // すでにデータベースに登録済みの課題について、登録されているdisplayとfinishの値をもってくる
+  const tasks_reg = (await db.collection("tasks").doc(user_id).get()).data();
+  const new_task_data = {...task_data_general, ...task_data_specific};
+  Object.keys(new_task_data).forEach((key) => {
+    if (key in tasks_reg){
+      new_task_data[key].finish = tasks_reg[key].finish;
+      new_task_data[key].display = tasks_reg[key].display;
+    }
+  });
+
+  // データベースにのみ存在する課題（手動追加課題）をマージする
+  Object.keys(tasks_reg).forEach((key) => {
+    if (!(key in new_task_data)){
+      new_task_data[key] = tasks_reg[key];
+    }
+  })
+
 
   // Firestore保存形式をflexデータに変換
   const flex_data = data_formatter.json_to_flex({
-    tasks: {...task_data_general, ...task_data_specific}
+    tasks: new_task_data
   });
 
-  if (Object.keys({...task_data_general, ...task_data_specific}).length == 0){
+  if (Object.keys(new_task_data).length == 0){
     return Promise.resolve({result: "no task"})
 
   } else {
     // Firestoreに保存
-    db.collection("tasks").doc(user_id).update({...task_data_general, ...task_data_specific});
+    db.collection("tasks").doc(user_id).set(new_task_data ,{merge: true});
     return Promise.resolve({result: "ok", data: flex_data});
   }
 }

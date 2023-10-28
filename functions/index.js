5bdea4f7acfdf94baf4a3300cee18eaf0d1ada25
yuto0727 <wy0727.watyu20@gmail.com>
2023-10-28 16:35:21 +0900

    ユーザーデータ取得の遅延を改善

--- functions/index.js
+++ functions/index.js
@@ -17,17 +17,45 @@ const linebot_client = new linebot_sdk.Client(linebot_account);
 // ExpressApp作成
 const express = require("express");
 const app = express();
-const admin_app = express();
 
 // データベースインスタンス作成
 const db = getFirestore();
 
 // ----------------------------------------------
-// エンドポイント公開設定
+// 連携済み全ユーザーデータ取得関数
+// ----------------------------------------------
+const get_all_data = async({get_class_name_dic=false}) => {
+  const all_user_data = {}, all_reg_tasks = {};
+  (await db.collection("users").get()).forEach(doc => {
+    const data = doc.data();
+    if (data.account_status == "linked"){
+      all_user_data[doc.id] = data;
+    }
+  });
+  (await db.collection("tasks").get()).forEach(doc => {
+    if (doc.id in all_user_data){
+      all_reg_tasks[doc.id] = doc.data();;
+    }
+  });
+  const all_user_id = Object.keys(all_user_data);
+  if (get_class_name_dic){
+    const class_name_dic = (await db.collection("overall").doc("classes").get()).data();
+    return {all_user_data: all_user_data, all_reg_tasks:all_reg_tasks, all_user_id:all_user_id, class_name_dic:class_name_dic};
+  } else {
+    return {all_user_data: all_user_data, all_reg_tasks:all_reg_tasks, all_user_id:all_user_id};
+  }
+}
+
+// ----------------------------------------------
+// エンドポイント内部処理設定
 // ----------------------------------------------
 app.use("/webhook", linebot_sdk.middleware(linebot_account));
 app.post("/webhook", (req, res) => {
-  console.log("<<<<<<<-----------------------処理開始----------------------->>>>>>>");
+  try{
+    console.log(`webhook処理開始 from: [${req.body.events[0].source.userId}] msg: [${(req.body.events[0].message.text).replace(/\n/g, "")}]`);
+  } catch(e) {
+    console.log(`webhook処理開始 from: [${req.body.events[0].source.userId}] type: [${req.body.events[0].type}]`);
+  }
   console.time("レスポンス処理所要時間");
 
   const line_sender = new Line_Sender({
@@ -48,110 +76,64 @@ app.post("/webhook", (req, res) => {
   })
 
   console.timeEnd("レスポンス処理所要時間");
-  console.log(">>>>>>>-----------------------処理終了-----------------------<<<<<<<");
-});
-
-admin_app.get("/:id/task_update/:method", async(req, res) => {
-  const admin_ids = (await db.collection("overall").doc("admin").get()).data().id;
-  if (admin_ids.includes(req.params.id)){
-    if (req.params.method == "all"){
-      const app_auto_task_update = require("./apps/app_auto_task_update");
-      app_auto_task_update(db)
-      .then((r) => {
-        console.log("finish", r)
-      }).catch((e) => {
-        console.log(e);
-      });
-      res.status(200).send("start app_auto_task_update");
-
-    } else {
-      res.status(200).send(`Error: invalid method of ${req.params.method}`);
-    }
-  } else {
-    res.status(200).send(`Error: invalid id of ${req.params.id}`);
-  }
-});
-
-admin_app.get("/:id/task_notify/:method", async(req, res) => {
-  const admin_ids = (await db.collection("overall").doc("admin").get()).data().id;
-  if (admin_ids.includes(req.params.id)){
-    if (req.params.method == "all"){
-      const app_auto_notify = require("./apps/app_auto_notify");
-      app_auto_notify(db)
-      .then((r) => {
-        console.log("finish", r)
-      }).catch((e) => {
-        console.log(e);
-      });
-      res.status(200).send("start app_auto_notify");
-
-    } else {
-      res.status(200).send(`Error: invalid method of ${req.params.method}`);
-    }
-  } else {
-    res.status(200).send(`Error: invalid id of ${req.params.id}`);
-  }
 });
 
 
+// ----------------------------------------------
+// エンドポイント公開設定
+// ----------------------------------------------
 exports.line_end_point = functions
 .region('asia-northeast1')
 .runWith({
   maxInstances: 10,
-  memory: "1GB",
+  memory: "2GB",
   secrets: ["R_LIST_MENU", "R_LIST_MENU_OVERLAY", "MAIL_PASS"]
 })
 .https
 .onRequest(app);
 
-exports.admin = functions
-.region('asia-northeast1')
-.runWith({
-  maxInstances: 10,
-  memory: "1GB",
-  secrets: ["MAIL_PASS"]
-})
-.https
-.onRequest(admin_app);
 
-exports.auto_update = functions
+// ----------------------------------------------
+// 定期実行関数設定：課題アップデート
+// ----------------------------------------------
+exports.trigger_update = functions
 .region('asia-northeast1')
 .runWith({
-  maxInstances: 10,
-  memory: "1GB",
-  timeoutSeconds: 300
+  maxInstances: 1,
+  memory: "1GB"
 })
 .pubsub.schedule('every day 8:30')
+// .pubsub.schedule('every day 16:16')
 .timeZone('Asia/Tokyo')
 .onRun(async(context) => {
-  const app_auto_task_update = require("./apps/app_auto_task_update");
-  app_auto_task_update(db)
-  .then((res) => {
-    console.log("finish", res)
-  }).catch((e) => {
-    console.log(e);
+  const autoapp_update = require("./apps/autoapp_update");
+  autoapp_update(db, await get_all_data({ get_class_name_dic : true }))
+  .catch((e) => {
+    console.log("自動更新でエラー発生", e);
   });
-
   return null;
 });
 
-exports.auto_notify = functions
+
+// ----------------------------------------------
+// 定期実行関数設定：課題通知
+// ----------------------------------------------
+exports.trigger_notify = functions
 .region('asia-northeast1')
 .runWith({
-  maxInstances: 10,
+  maxInstances: 1,
   memory: "1GB",
-  timeoutSeconds: 300,
-  secrets: ["MAIL_PASS"]
+  secrets: ["MAIL_PASS"],
+  timeoutSeconds: 540
 })
 .pubsub.schedule('every day 9:00')
+// .pubsub.schedule('every day 16:25')
 .timeZone('Asia/Tokyo')
 .onRun(async(context) => {
-  const app_auto_notify = require("./apps/app_auto_notify");
-  app_auto_notify(db)
-  .then((res) => {
-    console.log("finish", res)
-  }).catch((e) => {
-    console.log(e);
+  const autoapp_notify = require("./apps/autoapp_notify");
+  autoapp_notify(await get_all_data({ get_class_name_dic : false }))
+  .catch((e) => {
+    console.log("自動通知でエラー発生", e);
   });
 
   return null;

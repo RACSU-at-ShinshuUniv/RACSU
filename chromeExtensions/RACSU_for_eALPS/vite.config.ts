ce4ea018825951f16095bcfc32f08bf50d39a9d2
yuto0727 <wy0727.watyu20@gmail.com>
2024-05-11 02:39:39 +0900

    設定を修正

--- chromeExtensions/RACSU_for_eALPS/vite.config.ts
+++ chromeExtensions/RACSU_for_eALPS/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { crx, defineManifest } from '@crxjs/vite-plugin'
 
 const manifest = defineManifest({
-  name: "RACSU for eALPS",
+  name: "DevMode | RACSU for eALPS",
   description: "eALPS登録課題を一覧表示します。この拡張機能は、「eALPS支援ツール」として信州大学e-Learningセンターに認定申請中です。",
   version: "1.1.0",
   manifest_version: 3,
@@ -14,7 +14,14 @@ const manifest = defineManifest({
     48: "icon/icon48.png",
     128: "icon/icon128.png"
   },
-  options_page: "pages/popup/popup.html"
+  background: {
+    service_worker: "src/background.ts",
+    type: "module"
+  },
+  action: {
+    default_popup: "pages/popup/index.html"
+  },
+  options_page: "pages/popup/index.html"
 });
 
 export default defineConfig({
@@ -26,9 +33,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        popup: "pages/popup/index.html",
-      },
+        // defineManifestで指定したファイルは自動でコンパイル対象になる
+        // それ以外にコンパイルしたいファイルがあれば記述
+      }
     },
   },
 });

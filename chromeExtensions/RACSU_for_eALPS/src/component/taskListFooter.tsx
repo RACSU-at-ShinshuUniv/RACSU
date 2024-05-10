9f25db69dbdc0918d8cd231e33f907219ff32f62
yuto0727 <wy0727.watyu20@gmail.com>
2024-05-11 02:43:02 +0900

    操作項目をメニューにまとめるデザインに変更

--- chromeExtensions/RACSU_for_eALPS/src/component/taskListFooter.tsx
+++ chromeExtensions/RACSU_for_eALPS/src/component/taskListFooter.tsx
@@ -2,53 +2,36 @@
 import { css } from '@emotion/react';
 import color from "../color.json";
 
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 const style = {
-  button_delete_finish: css`
-    margin-right: 4px;
-    font-size: 12px;
-    background-color: ${color.green};
-    :hover {
-      background-color: ${color.green_hover};
+  fab: css`
+    position: absolute;
+    background-color: ${color.blue_checked};
+    width: 45px;
+    height: 45px;
+    bottom: 12px;
+    right: 12px;
+
+    & .MuiSvgIcon-root {
+      color: #ffffff;
     }
-  `,
 
-  button_delete_past: css`
-  font-size: 12px;
-  background-color: ${color.wine};
-  :hover {
-    background-color: ${color.wine_hover};
-  }
-  `,
-
-  warning: css`
-    color: ${color.warning};
-    font-weight: bold;
-    font-size: 13px;
-  `,
-
-  info: css`
-    font-size: 12px;
-  `,
-}
+    &:hover {
+      background-color: ${color.blue};
+    }
+  `
+};
 
 type props = {
-  warningMessage: string,
-  modalHandler_A: (isOpen: boolean) => void,
-  modalHandler_B: (isOpen: boolean) => void
+  addHandler: () => void
 }
 
-export default function Footer({warningMessage, modalHandler_A, modalHandler_B}: props) {
+export default function Footer({addHandler}: props) {
   return (
-    <Box display="flex" alignItems="center" padding="5px 16px" height="fit-content" borderTop={`1px solid ${color.frame_border}`}>
-      <Box marginRight="auto" marginTop="auto">
-        <p css={style.warning}>{warningMessage}</p>
-        <p css={style.info}>※課題の完了情報はeALPSと同期されません。</p>
-      </Box>
-      <Button css={style.button_delete_finish} onClick={() => modalHandler_A(true)} variant="contained">完了済みをすべて削除</Button>
-      <Button css={style.button_delete_past} onClick={() => modalHandler_B(true)} variant="contained">超過をすべて削除</Button>
-    </Box>
+    <Fab aria-label="add" css={style.fab} onClick={addHandler}>
+      <AddIcon />
+    </Fab>
   );
 }
--- chromeExtensions/RACSU_for_eALPS/src/component/taskListHeader.tsx
+++ chromeExtensions/RACSU_for_eALPS/src/component/taskListHeader.tsx
@@ -2,11 +2,19 @@
 import { css } from '@emotion/react';
 import color from "../color.json";
 
+import React from 'react';
+
 import IconButton from '@mui/material/IconButton';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import AddIcon from '@mui/icons-material/Add';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 const style = {
   title: css`
@@ -15,44 +23,106 @@ const style = {
     margin-left: 10px;
   `,
 
-  icon_refresh: css`
-    color: ${color.green};
+  button_other: css`
+    color: ${color.gray};
   `,
 
-  icon_add: css`
-    color: ${color.gray};
-    width: 28px;
-    height: 28px;
-  `,
+  menu_item: css`
+    & .MuiPaper-root {
+      border-radius: 6;
+      min-width: 180;
 
-  icon_setting: css`
-    color: ${color.gray};
+      & .MuiMenu-list {
+        padding: 4px 0;
+      }
+
+      & .MuiMenuItem-root {
+        & .MuiSvgIcon-root {
+          color: ${color.gray};
+          font-size: 18;
+          margin-right: 10px;
+        }
+      }
+    }
   `
 };
 
 type props = {
-  updateAt: string,
+  lastUpdate: string,
   refreshHandler: () => void,
-  addHandler: () => void,
+  confirmDelPastHandler: () => void,
+  confirmDelFinishHandler: () => void,
   settingHandler: () => void
 }
 
-export default function Header({updateAt, refreshHandler, addHandler, settingHandler}: props) {
+export default function Header({lastUpdate, refreshHandler, confirmDelPastHandler, confirmDelFinishHandler, settingHandler}: props) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (type: "refresh" | "delPast" | "delFinish" | "setting" | "close") => {
+    switch (type) {
+      case "refresh":
+        refreshHandler()
+        break;
+
+      case "delFinish":
+        confirmDelFinishHandler()
+        break;
+
+      case "delPast":
+        confirmDelPastHandler()
+        break;
+
+      case "setting":
+        settingHandler()
+        break;
+
+      case "close":
+        break;
+    }
+    setAnchorEl(null);
+  };
+
   return (
     <Box display="flex" alignItems="center" padding="0 10px" borderBottom={`1px solid ${color.frame_border}`}>
       <img width="35px" height="35px" src="/icon/icon32.png" alt="RACSU Logo" />
       <p css={style.title}>
-        eALPS 登録課題一覧（最終更新 {updateAt}）
+        eALPS 登録課題一覧（最終更新 {lastUpdate}）
       </p>
-      <IconButton sx={{marginRight: "auto"}} onClick={() => refreshHandler()}>
-        <RefreshIcon css={style.icon_refresh} />
-      </IconButton>
-      <IconButton onClick={() => addHandler()}>
-        <AddIcon css={style.icon_add} />
-      </IconButton>
-      <IconButton onClick={() => settingHandler()}>
-        <SettingsOutlinedIcon css={style.icon_setting} />
-      </IconButton>
+      <Box marginLeft="auto">
+        <IconButton css={style.button_other} onClick={handleClick}>
+          <MoreVertIcon css={css`color: ${color.gray};`} />
+        </IconButton>
+        <Menu
+          id="menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={() => handleClose("close")}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }} // アンカーに接地する位置
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} // アンカー位置
+          css={style.menu_item}
+        >
+          <MenuItem onClick={() => handleClose("refresh")}>
+            <RefreshIcon />
+            リストを更新
+          </MenuItem>
+          <MenuItem onClick={() => handleClose("delFinish")}>
+            <DeleteIcon />
+            完了課題を非表示
+          </MenuItem>
+          <MenuItem onClick={() => handleClose("delPast")}>
+            <AutoDeleteIcon />
+            超過課題を非表示
+          </MenuItem>
+          <Divider sx={{ my: 0.5 }} />
+          <MenuItem onClick={() => handleClose("setting")}>
+            <SettingsOutlinedIcon />
+            設定
+          </MenuItem>
+        </Menu>
+      </Box>
     </Box>
-  );
+  )
 }

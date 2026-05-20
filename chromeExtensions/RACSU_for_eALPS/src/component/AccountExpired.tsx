6d4454a889ae24d7a511d876ba87c6e2be42e9cf
yuto0727 <wy0727.watyu20@gmail.com>
2026-05-21 01:24:34 +0900

    お問い合わせリンク追加

--- chromeExtensions/RACSU_for_eALPS/src/component/AccountExpired.tsx
+++ chromeExtensions/RACSU_for_eALPS/src/component/AccountExpired.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import env from "../../env.json"
+import { css } from "@emotion/react";
+import env from "../../env.json";
 
-import Box from '@mui/material/Box';
-import LaunchIcon from '@mui/icons-material/Launch';
+import Box from "@mui/material/Box";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 const style = {
   overlay: css`
@@ -17,12 +17,16 @@ const style = {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    z-index: 10000;
+    z-index: 5000;
   `,
 
   info: css`
     font-size: 16px;
-    margin-left: 10px;
+  `,
+
+  info_help: css`
+    font-size: 14px;
+    margin-top: 20px;
   `,
 
   link: css`
@@ -41,23 +45,67 @@ const style = {
     &:hover {
       border-bottom: 1px solid ${env.color.blue};
     }
-  `
-}
+  `,
 
-export default function AccountExpired({isOpen, message, settingCallback}: {isOpen: boolean, message: string, settingCallback: () => void}) {
+  link_inline: css`
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    font-size: 14px;
+    color: ${env.color.blue};
+    margin-top: 5px;
+    border-bottom: 1px solid #00000000;
+
+    & .MuiSvgIcon-root {
+      font-size: 16px;
+    }
+
+    &:hover {
+      border-bottom: 1px solid ${env.color.blue};
+    }
+  `,
+};
+
+export default function AccountExpired({
+  isOpen,
+  message,
+  buttonMessage = "連携設定を開始する",
+  settingCallback,
+}: {
+  isOpen: boolean;
+  message: string;
+  buttonMessage?: string;
+  settingCallback: () => void;
+}) {
   if (isOpen) {
     return (
       <Box css={style.overlay}>
         <p css={style.info}>{message}</p>
         <Box css={style.link} onClick={() => settingCallback()}>
-          <p>連携設定を開始する</p>
+          <p>{buttonMessage}</p>
           <LaunchIcon />
         </Box>
+        <p css={style.info_help}>
+          うまくいかない場合は、
+          <Box css={style.link_inline} onClick={() => {
+            const optionsPage = chrome.runtime.getURL("pages/options/index.html");
+            window.open(optionsPage, "_blank");
+          }}>
+            <p>設定ページ</p>
+            <LaunchIcon />
+          </Box>
+          を確認、または
+          <Box css={style.link_inline} onClick={() => {
+            window.open(env.contactFormURL, "_blank");
+          }}>
+            <p>お問い合わせページ</p>
+            <LaunchIcon />
+          </Box>
+          よりお知らせください。
+        </p>
       </Box>
     );
   } else {
-    return (
-      <></>
-    );
+    return <></>;
   }
 }

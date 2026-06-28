8391e604cd4c97441223f9fc8c3c286f7d321d8b
yuto0727 <wy0727.watyu20@gmail.com>
2026-06-28 12:55:18 +0900

    シラバスからの授業名取得を一時的に無効化

--- chromeExtensions/RACSU_for_eALPS/src/modules/SyllabusClient.ts
+++ chromeExtensions/RACSU_for_eALPS/src/modules/SyllabusClient.ts
@@ -18,51 +18,57 @@ export class SyllabusClient {
       );
       return this.classNameDict[classCode];
     } else {
-      const classCodeFixed = (() => {
-        // 授業コードが9文字以上の場合は8文字でスライスして末尾2桁を00に置き換え
-        if (classCode.length >= 9) {
-          return classCode.slice(0, 6) + "00";
-        } else {
-          return classCode.slice(0, 8);
-        }
-      })();
-      const department = classCodeFixed.slice(0, 1).replace("Q", "G");
-      const term = new Date();
-      term.setMonth(term.getMonth() - 3);
-      const syllabusUrl = env.syllabusURL
-        .replace("$term", String(term.getFullYear()))
-        .replace("$department", department)
-        .replace("$classCode", classCodeFixed);
-
-      const syllabusContents = await (await fetch(syllabusUrl)).text();
-      const detectedClassName_A = syllabusContents
-        .replace(/\n|\r\n|\t| /g, "")
-        .match(/授業名<\/td><tdcolspan="7">(?<name>.*?)<\/td>/);
-      const detectedClassName_B = syllabusContents
-        .replace(/\n|\r\n|\t| /g, "")
-        .match(/科目名<\/td><tdcolspan="1">(?<name>.*?)<\/td>/);
-
-      const className = (() => {
-        if (
-          detectedClassName_A !== null &&
-          detectedClassName_A.groups !== undefined &&
-          detectedClassName_A.groups.name !== undefined
-        ) {
-          return detectedClassName_A.groups.name;
-        } else if (
-          detectedClassName_B !== null &&
-          detectedClassName_B.groups !== undefined &&
-          detectedClassName_B.groups.name !== undefined
-        ) {
-          return detectedClassName_B.groups.name;
-        } else {
-          return `不明な授業（${classCode}）`;
-        }
-      })();
-
-      console.log(`シラバスから取得：${classCode}->${className}`);
+      const className = `不明な授業（${classCode}）`;
       this.classNameDict[classCode] = className;
       return className;
+
+      // 2026/06/28 キャンパス情報システムの仕様変更で、以下の授業名取得処理が使えなくなったため、修正までコメントアウト
+
+      // const classCodeFixed = (() => {
+      //   // 授業コードが9文字以上の場合は8文字でスライスして末尾2桁を00に置き換え
+      //   if (classCode.length >= 9) {
+      //     return classCode.slice(0, 6) + "00";
+      //   } else {
+      //     return classCode.slice(0, 8);
+      //   }
+      // })();
+      // const department = classCodeFixed.slice(0, 1).replace("Q", "G");
+      // const term = new Date();
+      // term.setMonth(term.getMonth() - 3);
+      // const syllabusUrl = env.syllabusURL
+      //   .replace("$term", String(term.getFullYear()))
+      //   .replace("$department", department)
+      //   .replace("$classCode", classCodeFixed);
+
+      // const syllabusContents = await (await fetch(syllabusUrl)).text();
+      // const detectedClassName_A = syllabusContents
+      //   .replace(/\n|\r\n|\t| /g, "")
+      //   .match(/授業名<\/td><tdcolspan="7">(?<name>.*?)<\/td>/);
+      // const detectedClassName_B = syllabusContents
+      //   .replace(/\n|\r\n|\t| /g, "")
+      //   .match(/科目名<\/td><tdcolspan="1">(?<name>.*?)<\/td>/);
+
+      // const className = (() => {
+      //   if (
+      //     detectedClassName_A !== null &&
+      //     detectedClassName_A.groups !== undefined &&
+      //     detectedClassName_A.groups.name !== undefined
+      //   ) {
+      //     return detectedClassName_A.groups.name;
+      //   } else if (
+      //     detectedClassName_B !== null &&
+      //     detectedClassName_B.groups !== undefined &&
+      //     detectedClassName_B.groups.name !== undefined
+      //   ) {
+      //     return detectedClassName_B.groups.name;
+      //   } else {
+      //     return `不明な授業（${classCode}）`;
+      //   }
+      // })();
+
+      // console.log(`シラバスから取得：${classCode}->${className}`);
+      // this.classNameDict[classCode] = className;
+      // return className;
     }
   };
 
